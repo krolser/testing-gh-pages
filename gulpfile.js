c9941078c5e15759e13a1b047ea8913adcb7fa7f
krolser <krolser@gmail.com>
2023-01-21 21:50:50 +0300

    remove gh-pages deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 var gulp        = require('gulp');
-var ghPages = require('gh-pages');
 
 gulp.task('html', function () {
     return gulp.src("*.html")
         .pipe(gulp.dest("dist/"));
-});
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return ghPages.publish('dist');
 });
